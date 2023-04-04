11729c58817067a09041a85bdf0fbc329e065505
seheeeJeong <usung7888@naver.com>
2023-04-04 18:10:58 +0900

    mvc

---
+++ 12_mvc/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const indexRouter = require("./routes");
+// require: 폴더까지만 접근할 경우엔, 자동으로 index.js 파일을 찾아감
+app.use("/", indexRouter);
+// '/~~~~' 모든 라우터를 /routes/index.js 에 정의해 놨으니, /routes/index.js
+
+// [404 error]
+// 맨 마지막 라우트로 선언: nor 나머지 코드 무시되기 때문!!
+app.get("*", (req, res) => {
+  // res.send('404 Error! 잘못된 주소 형식입니다.');
+  res.render("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
--- 12_mvc/app_before.js
+++ 12_mvc/app_before.js
@@ -1,38 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/static', express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // (임시) DB로부터 받아온 댓글 목록
 const comments = [
+  //comments = 배열
   {
     id: 1,
-    userid: 'helloworld',
-    date: '2022-10-31',
-    comment: '안녕하세요^~^',
+    userid: "helloworld",
+    date: "2022-10-31",
+    comment: "안녕하세요^~^",
   },
   {
     id: 2,
-    userid: 'happy',
-    date: '2022-11-01',
-    comment: '반가워유',
+    userid: "happy",
+    date: "2022-11-01",
+    comment: "반가워유",
   },
   {
     id: 3,
-    userid: 'lucky',
-    date: '2022-11-02',
-    comment: '오 신기하군',
+    userid: "lucky",
+    date: "2022-11-02",
+    comment: "오 신기하군",
   },
   {
     id: 4,
-    userid: 'bestpart',
-    date: '2022-11-02',
-    comment: '첫 댓글입니당ㅎㅎ',
+    userid: "bestpart",
+    date: "2022-11-02",
+    comment: "첫 댓글입니당ㅎㅎ",
   },
 ];
 
@@ -40,18 +41,24 @@ const comments = [
 // 단점: 라우터(경로)가 많아진다면? app.js 코드가 길어짐 -> 유지보수성 하락
 
 // GET /
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 // GET /comments
-app.get('/comments', (req, res) => {
+app.get("/comments", (req, res) => {
   console.log(comments); // 댓글 목록이 [ {}, {}, {}, {} ] 형태로 출력
-  res.render('comments', { commentInfos: comments });
+  res.render("comments", { commentInfos: comments });
 });
 
 // GET /comment/:id
-app.get('/comment/:id', (req, res) => {
+// GET /comment
+// /comment?id=6&name=홍길동
+// req.query { id: 6, name: '홍길동' } // req.query.id => 6
+// GET /comment/:id/name
+// >comment/6/홍길동
+// req.params { id : 6, name: '홍길동' } // req.params.id => 6
+app.get("/comment/:id", (req, res) => {
   console.log(req.params); // 라우트 매개변수에 대한 정보 담겨 있음
   console.log(req.params.id); // id 고유 값
 
@@ -60,23 +67,23 @@ app.get('/comment/:id', (req, res) => {
 
   // 존재하지 않는 댓글 id 접근시 404 페이지
   if (commentId < 1 || commentId > comments.length) {
-    return res.render('404');
+    return res.render("404");
   }
 
   // :id 변수에 숫자가 아닌 값이 온다면 404 페이지
   if (isNaN(commentId)) {
-    return res.render('404');
+    return res.render("404");
   }
 
-  res.render('comment', { commentInfo: comments[commentId - 1] });
+  res.render("comment", { commentInfo: comments[commentId - 1] });
 });
 
-// [404 error]
+// [404 error] // 맨 마지막에 쓰기
 // 맨 마지막 라우트로 선언: nor 나머지 코드 무시되기 때문!!
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   // res.send('404 Error! 잘못된 주소 형식입니다.');
-  res.render('404');
-});
+  res.render("404");
+}); // '*' = 전체 문자를 의미.
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
